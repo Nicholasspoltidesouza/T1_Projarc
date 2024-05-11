5c7be0abce4a314253fd2411fba3c5acbad7f523
Nicholas Spolti de Souza <nicholasspoltidesouza@gmail.com>
2024-05-11 14:26:35 -0300

    setup

---
+++ src/main/java/com/leet/t1_clean/T1CleanApplication.java
@@ -0,0 +1,13 @@
+package com.leet.t1_clean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1CleanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T1CleanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leet/t1_clean/T1CleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leet.t1_clean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1CleanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
