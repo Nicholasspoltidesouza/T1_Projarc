fabe9b1685816987089b1723625e48923f85e105
Nicholas Spolti <nicholasspoltidesouza@gmail.com>
2024-06-03 18:38:43 -0300

    a

---
+++ src/main/java/com/projarc/clean/CleanApplication.java
@@ -0,0 +1,15 @@
+package com.projarc.clean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.projarc.clean.application.mapper")
+public class CleanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanApplication.class, args);
+    }
+
+}
